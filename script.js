c8a36d05ef0032590d14ef02160ccd9f6cb172cd
marcellopfc <51771835+marcellopfc@users.noreply.github.com>
2023-05-04 02:06:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,21 +2,30 @@ const apiKey = 'QxExxRp47SYf1DJsRz5oLWaYNUGtgSzx';
 const currentGif = document.getElementById('current-gif');
 let offset = 0;
 
-function loadGif() {
-	fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=1&offset=${offset}`)
+
+function loadGif(query) {
+	let url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=1&offset=${offset}`;
+	if (query) {
+		url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=1&offset=${offset}`;
+	}
+
+	fetch(url)
 		.then(response => response.json())
 		.then(data => {
+			if (data.data.length === 0) {
+				alert(`No results found for "${query}"`);
+				return;
+			}
 			currentGif.src = data.data[0].images.original.url;
 			currentGif.alt = data.data[0].title;
 		})
 		.catch(error => console.error(error));
 }
-
 function handleArrowKeys(event) {
 	if (event.key === 'ArrowLeft') {
 		offset = Math.max(0, offset - 1);
 		loadGif();
-	} else if (event.key === 'ArrowRight') {
+	} else if (event.key === 'ArrowRight'|| event.key===' ') {
 		offset++;
 		loadGif();
 	}
@@ -28,7 +37,16 @@ function handleMouseClick(event) {
 		loadGif();
 	}
 }
-
+document.getElementById('search-input').addEventListener('keypress', event => {
+	if (event.key === 'Enter') {
+		handleSearch();
+	}
+});
+function handleSearch() {
+	const query = document.getElementById('search-input').value;
+	offset = 0;
+	loadGif(query);
+}
 document.addEventListener('keydown', handleArrowKeys);
 document.addEventListener('click', handleMouseClick);
 
